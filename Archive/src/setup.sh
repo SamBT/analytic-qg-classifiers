9466dd8b1844a15d63f2971c736b0597fe34893a
Samuel Bright-Thonney <paperman50@gmail.com>
2020-08-10 08:40:22 -0700

    small updates; learning

---
+++ Archive/src/setup.sh
@@ -0,0 +1,13 @@
+setup_PYTHIA() {
+    export PYTHIA8LOCATION=/global/project/projectdirs/atlas/sambt/MC_Generators/pythia8244/
+    export PYTHIA8DATA=${PYTHIA8LOCATION}/share/Pythia8/xmldoc
+    export LD_LIBRARY_PATH=${PYTHIA8LOCATION}/lib/:$LD_LIBRARY_PATH
+}
+
+setup_fastjet() {
+    export FASTJETLOCATION=/global/project/projectdirs/atlas/sambt/MC_Generators/fastjet-install
+    export LD_LIBRARY_PATH=/global/project/projectdirs/atlas/sambt/MC_Generators/fastjet-install/lib:$LD_LIBRARY_PATH
+}
+
+setup_PYTHIA
+setup_fastjet
