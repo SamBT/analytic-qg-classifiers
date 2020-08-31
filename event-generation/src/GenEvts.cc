1c795ee92c739f263eb828ebedb4796558c71c52
Sam Bright-Thonney <sbright@berkeley.edu>
2020-08-31 10:15:10 -0700

    added kernel order to GenEvts arguments

--- event-generation/src/GenEvts.cc
+++ event-generation/src/GenEvts.cc
@@ -3,9 +3,6 @@
 //   Pythia generation of e+e- > qqbar and e+e- > ggbar through an intermediate Higgs
 //          based on a modified version of the main06.cc Pythia example script
 //			and the dire05.cc Dire example script
-//
-//          Eric M. Metodiev, MIT, 2018 - 2019
-//
 //    g++ events.cc -o events `/Users/metodiev/bin/dire-config --all-libs`
 //    g++ events.cc -o events `/Users/metodiev/pythia8235/examples/direforpythia-66862c4022ad6958140db46680af723723acfe26/trunk/bin/dire-config --all-libs`
 //    ./events dire.cmnd -nev 1000 -proc 1 -ecm 200 -CF 1.333333 -CA 3.000000
@@ -30,15 +27,17 @@ int main( int argc, char* argv[] ) {
   int nev = 1000;
   double CF = 1.33333;
   double CA = 3.00000;
+  int kernelOrder = 3;
   if (argc < 5) {
-    cout << "Too few arguments! Usage : ./events dire-config.cmnd CF CA nEvents" << endl;
+    cout << "Too few arguments! Usage : ./events dire-config.cmnd nEvents CF CA kernelOrder" << endl;
     return 0;
   }
   else {
     //config = argv[1];
-    CF = atof(argv[2]);
-    CA = atof(argv[3]);
-    nev = atoi(argv[4]);
+    nev = atoi(argv[2])
+    CF = atof(argv[3]);
+    CA = atof(argv[4]);
+    kernelOrder = atoi(argv[5]);
   }
 
   // initialize Pythia and Dire
@@ -47,18 +46,18 @@ int main( int argc, char* argv[] ) {
   EventHandler Analysis(0.4,20);
 
   //Pythia settings not covered in DIRE config file
-  //pythia.readString("Main:numberOfEvents = "+argv[2])
+
   // no substructure in e+e- beams
   pythia.readString("PDF:lepton = off");
   // set the quark casimir value
   pythia.settings.parm("DireColorQCD:CF", CF);
   // set the gluon casimir value
   pythia.settings.parm("DireColorQCD:CA", CA);
+  //set the kernel order
+  pythia.settings.parm("DireTimes:kernelOrder",kernelOrder);
 
   // initialize Dire
   dire.init(pythia, argv[1]);
-
-  //pythia.init(); //necessary?
 
   // Loop to generate events
   Analysis.Begin();
