466ad40f129647294930583d7d18ecb35447ba8c
Samuel Bright-Thonney <paperman50@gmail.com>
2020-09-01 11:18:24 -0700

    batch submission for CA-CF scan

--- event-generation/batchSub/run_job.sh
+++ event-generation/batchSub/run_job.sh
@@ -13,18 +13,23 @@ date
 pythdir=/global/project/projectdirs/atlas/sambt/Pythia_output/optimal-classifiers
 mkdir ${pythdir}/${proc}
 
-rm -rf ${pythdir}/${proc}/${jobname}_${j}
-mkdir ${pythdir}/${proc}/${jobname}_${j}
-cd ${pythdir}/${proc}/${jobname}_${j}
+mkdir ${pythdir}/${proc}/kernel${kern}
+mkdir ${pythdir}/${proc}/kernel${kern}/${jobname}
+rm -rf ${pythdir}/${proc}/kernel${kern}/${jobname}/output_${j}
+mkdir ${pythdir}/${proc}/kernel${kern}/${jobname}/output_${j}
+cd ${pythdir}/${proc}/kernel${kern}/${jobname}/output_${j}
 
 module load root
+
+echo "in dir $PWD"
 
 cp /global/homes/s/sambt/Jets/optimal-classifiers/event-generation/src/GenEvts .
 
 config_dir=/global/homes/s/sambt/Jets/optimal-classifiers/event-generation/DIRE-config-files
 ./GenEvts ${config_dir}/${proc}.cmnd ${nev} ${CF} ${CA} ${kern}
 
-mv test.root ${filename}_${j}.root
+mv test.root ${pythdir}/${proc}/kernel${kern}/${jobname}/${filename}_${j}.root
+rm -rf ${pythdir}/${proc}/kernel${kern}/${jobname}/output_${j}
 
 cd ;
 date
