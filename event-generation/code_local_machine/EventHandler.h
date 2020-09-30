3d377cb1d8130f8cb1e80a8314df78b5b8ea90b7
Sam Bright-Thonney <sbright@berkeley.edu>
2020-09-30 12:45:17 -0400

    updated with local version of code

---
+++ event-generation/code_local_machine/EventHandler.h
@@ -0,0 +1,134 @@
+#ifndef EventHandler_H
+#define EventHandler_H
+
+#include <vector>
+#include <math.h>
+#include <string>
+#include <sstream>
+#include <set>
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TClonesArray.h"
+#include "TParticle.h"
+#include "TH1.h"
+#include "TH1F.h"
+#include "TCanvas.h"
+#include "TMath.h"
+#include "TVector3.h"
+#include "TRandom3.h"
+
+#include "Pythia8/Pythia.h"
+
+#include "fastjet/ClusterSequence.hh"
+#include "fastjet/PseudoJet.hh"
+#include "fastjet/tools/Filter.hh"
+#include "fastjet/Selector.hh"
+
+using namespace std;
+
+class EventHandler {
+  private:
+    //Tree & ROOT output objects
+    TTree *T;
+    TFile *F;
+    string outName;
+
+    //Event variables
+    int EventNumber;
+
+    //Jet variables
+    static const int max_njets = 20;
+    double pTmin = 20;
+    double jet_radius;
+
+    int nJetsFilled;
+    int nQJetsFilled;
+    int nGJetsFilled;
+
+    int njets;
+    double jet_pt[max_njets];
+    double jet_eta[max_njets];
+    double jet_phi[max_njets];
+    double jet_m[max_njets];
+    int jet_mult[max_njets];
+    bool is_qjet[max_njets];
+    bool is_gjet[max_njets];
+
+    //Quark jet variables
+    int nqjets;
+    double qjet_pt[max_njets];
+    double qjet_eta[max_njets];
+    double qjet_phi[max_njets];
+    double qjet_m[max_njets];
+    int qjet_mult[max_njets];
+
+    //Gluon jet variables
+    int ngjets;
+    double gjet_pt[max_njets];
+    double gjet_eta[max_njets];
+    double gjet_phi[max_njets];
+    double gjet_m[max_njets];
+    int gjet_mult[max_njets];
+
+    //Parton Jet Variables
+    int npJetsFilled;
+    int npQJetsFilled;
+    int npGJetsFilled;
+
+    int npjets;
+    double pjet_pt[max_njets];
+    double pjet_eta[max_njets];
+    double pjet_phi[max_njets];
+    double pjet_m[max_njets];
+    int pjet_mult[max_njets];
+    double plead_constit_pt[100];
+    double plead_constit_eta[100];
+    double plead_constit_phi[100];
+    double plead_constit_e[100];
+    bool is_pqjet[max_njets];
+    bool is_pgjet[max_njets];
+
+    //Quark parton-jet variables
+    int npqjets;
+    double pqjet_pt[max_njets];
+    double pqjet_eta[max_njets];
+    double pqjet_phi[max_njets];
+    double pqjet_m[max_njets];
+    int pqjet_mult[max_njets];
+    double pqlead_constit_pt[100];
+    double pqlead_constit_eta[100];
+    double pqlead_constit_phi[100];
+    double pqlead_constit_e[100];
+
+    //Gluon parton-jet variables
+    int npgjets;
+    double pgjet_pt[max_njets];
+    double pgjet_eta[max_njets];
+    double pgjet_phi[max_njets];
+    double pgjet_m[max_njets];
+    int pgjet_mult[max_njets];
+    double pglead_constit_pt[100];
+    double pglead_constit_eta[100];
+    double pglead_constit_phi[100];
+    double pglead_constit_e[100];
+
+    //Fastjet objects
+    fastjet::JetDefinition *m_jet_def; //Regular anti-kT jets
+    fastjet::JetDefinition *m_jet_def_part; //Parton-jets
+
+  public:
+    EventHandler (double radius, double min_pt);
+    ~EventHandler ();
+    void Begin();
+    void AnalyzeEvent(int iEvt, Pythia8::Pythia& pyth);
+    int JetType(fastjet::PseudoJet jet, vector<fastjet::PseudoJet> partons);
+    void End();
+    void DeclareBranches();
+    void ResetBranches();
+    void SetOutName(string fname){
+      outName = fname;
+    }
+};
+
+#endif
---
+++ event-generation/code_local_machine/GenEvts.cc
@@ -0,0 +1,72 @@
+//=============================================================================================
+//
+//   Pythia generation of e+e- > qqbar and e+e- > ggbar through an intermediate Higgs
+//          based on a modified version of the main06.cc Pythia example script
+//			and the dire05.cc Dire example script
+//    g++ events.cc -o events `/Users/metodiev/bin/dire-config --all-libs`
+//    g++ events.cc -o events `/Users/metodiev/pythia8235/examples/direforpythia-66862c4022ad6958140db46680af723723acfe26/trunk/bin/dire-config --all-libs`
+//    ./events dire.cmnd -nev 1000 -proc 1 -ecm 200 -CF 1.333333 -CA 3.000000
+//
+//=============================================================================================
+
+#include <iostream>
+#include <string.h>
+
+#include "Pythia8/Pythia.h"
+#include "EventHandler.h"
+
+using namespace Pythia8;
+using std::atoi;
+using std::atof;
+using std::cout;
+using std::endl;
+
+int main( int argc, char* argv[] ) {
+
+  int nev = 1000;
+  double CF = 1.33333;
+  double CA = 3.00000;
+  int kernelOrder = 3;
+  if (argc < 5) {
+    cout << "Too few arguments! Usage : ./events dire-config.cmnd nEvents CF CA kernelOrder" << endl;
+    return 0;
+  }
+  else {
+    //config = argv[1];
+    nev = atoi(argv[2]);
+    CF = atof(argv[3]);
+    CA = atof(argv[4]);
+    kernelOrder = atoi(argv[5]);
+  }
+
+  // initialize Pythia and Dire
+  Pythia pythia;
+  EventHandler Analysis(0.4,20);
+
+  //Pythia settings not covered in DIRE config file
+  // no substructure in e+e- beams
+  pythia.readFile(argv[1]);
+  pythia.readString("PDF:lepton = off");
+  // set the quark casimir value
+  pythia.settings.parm("DireColorQCD:CF", CF);
+  // set the gluon casimir value
+  pythia.settings.parm("DireColorQCD:CA", CA);
+  //set the kernel order
+  pythia.settings.mode("DireTimes:kernelOrder",kernelOrder);
+
+  pythia.init();
+
+  cout << "---------------------------STARTING EVENT GEN--------------------------- \n \n \n" << endl;
+
+  // Loop to generate events
+  Analysis.Begin();
+  for (int iev = 1; iev <= nev; iev++) {
+    // print a status update every 100 events
+    if (iev % 100 == 0) cout << "Generated " << iev << " events." << endl;
+
+    Analysis.AnalyzeEvent(iev,pythia);
+  }
+  Analysis.End();
+
+  return 0;
+}
