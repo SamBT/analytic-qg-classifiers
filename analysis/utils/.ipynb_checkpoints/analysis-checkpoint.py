e9417dd295939475bfa1cb67df3bc627b34a9e9f
Sam Bright-Thonney <skb93@cornell.edu>
2022-04-07 14:56:29 -0700

    analysis code from squirrel

--- analysis/utils/.ipynb_checkpoints/analysis-checkpoint.py
+++ analysis/utils/.ipynb_checkpoints/analysis-checkpoint.py
@@ -1,4 +1,151 @@
 import numpy as np
-def nk(z,k):
-    out = np.array([np.sum([f**k if f > 0 else 0 for f in j]) for j in z])
-    return out
+import train_tools as tools
+import plotting
+import os
+from sklearn.metrics import roc_auc_score, roc_curve
+
+def get_jet_data(s_quark,s_gluon,prefix='/home/sambt/pythia83-samples/optimal-classifiers/kernel-1/',
+                suffix='/total.root',nmax=-1,njets=-1,eta=-1):
+    fq = prefix+s_quark+suffix
+    fg = prefix+s_gluon+suffix
+    X,Y = tools.pre_process(fq,fg,nev_max=nmax,njets=njets,eta=eta,quiet=False)
+    z = X[:,:,0].copy()
+    dEta = X[:,:,1].copy()
+    dPhi = X[:,:,2].copy()
+    dR = np.sqrt(dEta**2 + dPhi**2)
+    mults = np.count_nonzero(z,axis=1)
+
+    return z,dEta,dPhi,dR,mults,Y
+
+def run_pipeline(s_quark,s_gluon,opt_func,prefix='/home/sambt/pythia83-samples/optimal-classifiers/kernel-1/',
+                suffix='/total.root',nmax=-1,njets=-1,eta=-1,verbose=0,summary=False,quiet=False,predicted="Predicted",
+                F_dropouts=0.2,epochs=3,modpath='/home/sambt/analytic-qg-classifiers/analysis/final-studies/saved_models/',savename=None):
+    fq = prefix+s_quark+suffix
+    fg = prefix+s_gluon+suffix
+    X,Y = tools.pre_process(fq,fg,nev_max=nmax,njets=njets,eta=eta,quiet=False)
+    z = X[:,:,0].copy()
+    mults = np.count_nonzero(z,axis=1)
+    optimal = opt_func(z)
+    
+    pfn, pfn_roc, pfn_auc = tools.train_qg_pfn(X,Y,n_epoch=epochs,summary=summary,verbose=verbose,F_dropouts=F_dropouts)
+    pfn_z, pfn_z_roc, pfn_z_auc = tools.train_qg_pfn_no_angular(X,Y,n_epoch=epochs,summary=summary,verbose=verbose,F_dropouts=F_dropouts)
+    pfn_noz, pfn_noz_roc, pfn_noz_auc = tools.train_qg_pfn_only_angular(X,Y,n_epoch=epochs,summary=summary,verbose=verbose,F_dropouts=F_dropouts)
+    efn, efn_roc, efn_auc = tools.train_qg_efn(X,Y,n_epoch=epochs,summary=summary,verbose=verbose,F_dropouts=F_dropouts)
+    
+    optimal_auc = roc_auc_score(Y[:,1],-optimal)
+    if optimal_auc < 0.5:
+        optimal_auc = roc_auc_score(Y[:,1],optimal)
+        optimal_roc = roc_curve(Y[:,1],optimal)
+    else:
+        optimal_roc = roc_curve(Y[:,1],-optimal)
+    
+    mult_auc = roc_auc_score(Y[:,1],mults)
+    if mult_auc < 0.5:
+        mult_auc = roc_auc_score(Y[:,1],-mults)
+        mult_roc = roc_curve(Y[:,1],-mults)
+    else:
+        mult_roc = roc_curve(Y[:,1],mults)
+    
+    rocs = [pfn_roc,pfn_z_roc,pfn_noz_roc,efn_roc,optimal_roc,mult_roc]
+    aucs = [pfn_auc,pfn_z_auc,pfn_noz_auc,efn_auc,optimal_auc,mult_auc]
+    labels = ["PFN",r"PFN[$z$]",r"PFN[$\eta$,$\phi$]","EFN",predicted,"Multiplicity"]
+    
+    del X,Y,z,mults,optimal,pfn,pfn_z,pfn_noz,efn
+
+    return rocs,aucs,labels
+
+def duffPS_run_pipeline(fq,fg,opt_func,prefix='/home/sambt/pythia83-samples/optimal-classifiers/kernel-1/',
+                suffix='/total.root',nmax=-1,njets=-1,eta=-1,verbose=1,summary=False,quiet=False,predicted="Predicted",
+                F_dropouts=0.2,epochs=3,modpath='/home/sambt/analytic-qg-classifiers/analysis/final-studies/saved_models/',savename=None):
+    X,Y = tools.duffPS_pre_process(fq,fg,nmax=nmax)
+    z = X[:,:,0].copy()
+    mults = np.count_nonzero(z,axis=1)
+    optimal = opt_func(z)
+    
+    epochs = 5
+    
+    pfn, pfn_roc, pfn_auc = tools.duffPS_train_pfn(X,Y,n_epoch=epochs,summary=summary,verbose=verbose,F_dropouts=F_dropouts)
+    pfn_z, pfn_z_roc, pfn_z_auc = tools.train_qg_pfn_no_angular(X,Y,n_epoch=epochs,summary=summary,verbose=verbose,F_dropouts=F_dropouts)
+    
+    optimal_auc = roc_auc_score(Y[:,1],-optimal)
+    if optimal_auc < 0.5:
+        optimal_auc = roc_auc_score(Y[:,1],optimal)
+        optimal_roc = roc_curve(Y[:,1],optimal)
+    else:
+        optimal_roc = roc_curve(Y[:,1],-optimal)
+    
+    mult_auc = roc_auc_score(Y[:,1],mults)
+    if mult_auc < 0.5:
+        mult_auc = roc_auc_score(Y[:,1],-mults)
+        mult_roc = roc_curve(Y[:,1],-mults)
+    else:
+        mult_roc = roc_curve(Y[:,1],mults)
+    
+    rocs = [pfn_roc,pfn_z_roc,optimal_roc,mult_roc]
+    aucs = [pfn_auc,pfn_z_auc,optimal_auc,mult_auc]
+    labels = [r"PFN[$z,\theta$]",r"PFN[$z$]",predicted,"Multiplicity"]
+    
+    del X,Y,z,mults,optimal,pfn
+
+    return rocs,aucs,labels
+
+def run_pfn(s_quark,s_gluon,opt_func,prefix='/home/sambt/pythia83-samples/optimal-classifiers/kernel-1/',
+                nmax=-1,njets=-1,eta=-1,verbose=0,summary=False,F_dropouts=0.2,suffix='/total.root'):
+    
+    fq = prefix+s_quark+suffix
+    fg = prefix+s_gluon+suffix
+    X,Y = tools.pre_process(fq,fg,nev_max=nmax,njets=njets,eta=eta)
+    z = X[:,:,0].copy()
+    mults = np.count_nonzero(z,axis=1)
+    optimal = opt_func(z)
+    
+    pfn, pfn_roc, pfn_auc = tools.train_qg_pfn(X,Y,n_epoch=3,summary=summary,verbose=verbose,F_dropouts=F_dropouts)
+    
+    optimal_auc = roc_auc_score(Y[:,1],-optimal)
+    if optimal_auc < 0.5:
+        optimal_auc = roc_auc_score(Y[:,1],optimal)
+        optimal_roc = roc_curve(Y[:,1],optimal)
+    else:
+        optimal_roc = roc_curve(Y[:,1],-optimal)
+    mult_roc = roc_curve(Y[:,1],mults)
+    mult_auc = roc_auc_score(Y[:,1],mults)
+    rocs = [pfn_roc,optimal_roc,mult_roc]
+    aucs = [pfn_auc,optimal_auc,mult_auc]
+    labels = ["PFN","Predicted","Multiplicity"]
+
+    del X,Y,z,mults,optimal,pfn
+    
+    return rocs,aucs,labels
+
+def run_custom_obs(s_quark,s_gluon,func,prefix='/home/sambt/pythia83-samples/optimal-classifiers/kernel-1/',
+                suffix='/total.root',nmax=-1,njets=-1,eta=-1,quiet=False):
+    
+    fq = prefix+s_quark+suffix
+    fg = prefix+s_gluon+suffix
+    X,Y = tools.pre_process(fq,fg,nev_max=nmax,njets=njets,eta=eta,quiet=quiet)
+    z = X[:,:,0].copy()
+    
+    if isinstance(func,list):
+        optimal_rocs,optimal_aucs = [],[]
+        for f in func:
+            optimal = f(z)
+            optimal_auc = roc_auc_score(Y[:,1],-optimal)
+            if optimal_auc < 0.5:
+                optimal_auc = roc_auc_score(Y[:,1],optimal)
+                optimal_roc = roc_curve(Y[:,1],optimal)
+            else:
+                optimal_roc = roc_curve(Y[:,1],-optimal)
+            optimal_rocs.append(optimal_roc)
+            optimal_aucs.append(optimal_auc)
+        del X,Y,z
+        return optimal_rocs,optimal_aucs
+    else:
+        optimal = func(z)
+        optimal_auc = roc_auc_score(Y[:,1],-optimal)
+        if optimal_auc < 0.5:
+            optimal_auc = roc_auc_score(Y[:,1],optimal)
+            optimal_roc = roc_curve(Y[:,1],optimal)
+        else:
+            optimal_roc = roc_curve(Y[:,1],-optimal)
+        del X,Y,z
+        return optimal_roc,optimal_auc
--- analysis/utils/.ipynb_checkpoints/plotting-checkpoint.py
+++ analysis/utils/.ipynb_checkpoints/plotting-checkpoint.py
@@ -35,29 +35,71 @@ def plot_rocs(pfn_roc,optimal_roc,mult_roc,pfn_auc,optimal_auc,mult_auc):
     plt.text(0.02,0.2,r"$C_F$ = $C_A$ = 3, Kernel Order = $-1$",fontsize=18)
     plt.show()
     
-def plot_many(rocs,aucs,labels):    
-    plt.rcParams['figure.figsize'] = (8,8)
+def plot_many(rocs,aucs,labels,ax,ncol=1,fontsize=16):    
     plt.rcParams['font.family'] = 'serif'
     plt.rcParams['figure.autolayout'] = True
+    plt.rcParams['text.usetex'] = True
     
     for i, roc in enumerate(rocs):
         fp, tp, threshs = roc
-        plt.plot(tp, 1-fp, '-', label=labels[i]+' ({0:.3f})'.format(aucs[i]))
+        ax.plot(tp, 1-fp, '-', label=labels[i]+' ({0:.3f})'.format(aucs[i]))
     
     #Draw 'random' line
-    plt.plot([0,1],[1,0],linestyle='dashed',color='gray')
+    ax.plot([0,1],[1,0],linestyle='dashed',color='gray')
 
     # axes labels
-    plt.xlabel('Quark Jet Efficiency',fontsize=14)
-    plt.ylabel('Gluon Jet Rejection',fontsize=14)
+    plt.xlabel('Quark Jet Efficiency',fontsize=fontsize)
+    plt.ylabel('Gluon Jet Rejection',fontsize=fontsize)
 
     # axes limits
     plt.xlim(0, 1)
     plt.ylim(0, 1)
-    plt.xticks(fontsize=12)
-    plt.yticks(fontsize=12)
+    plt.xticks(fontsize=fontsize-2)
+    plt.yticks(fontsize=fontsize-2)
 
     # make legend and show plot
-    plt.legend(loc='center left', frameon=False,fontsize=16)
-    plt.text(0.02,0.02,r"$C_F$ = $C_A$ = 3, Kernel Order = $-1$",fontsize=18)
-    plt.show()
+    plt.legend(loc='lower left', frameon=False,fontsize=fontsize,ncol=ncol)
+    #plt.show()
+    
+def stamp(left_x, top_y,radius=0.4,fontsize=16,
+          line_0=None,
+          line_1=r'\textsc{Pythia} 8.303 + \textsc{Dire}',
+          line_2=r'$e^+e^- \to H \to q\bar{q}/gg$',
+          line_3=r'$\sqrt{s} = 250$ GeV, $C_F = C_A = 3$',
+          line_4=r'Parton Level, R = 0.4',
+          delta_y=0.035,
+          textops_update={},
+          ax=None,
+          **kwargs):
+    
+    line_4 = r'Hadron Level, R = {0:.1f}'.format(radius)
+    textops = {'horizontalalignment': 'left', 'verticalalignment': 'center', 'fontsize': fontsize}
+    textops.update(textops_update)
+    if ax is None: ax = plt.gca()
+    textops['transform'] = ax.transAxes
+    kwargs.update({'line_0':line_0,'line_1': line_1, 'line_2': line_2, 'line_3': line_3, 'line_4':line_4})
+    for i in range(len(kwargs)):
+        y = top_y - i*delta_y
+        t = kwargs.get('line_' + str(i))
+        if t is not None:
+            ax.text(left_x, y, t, **textops)
+            
+def duffPS_stamp(left_x, top_y,radius=0.4,fontsize=16,
+          line_0=None,
+          line_1=r'Simple Parton Shower',
+          line_2=r'LL Splittings, $C_F = C_A = 3$',
+          delta_y=0.035,
+          textops_update={},
+          ax=None,
+          **kwargs):
+    
+    textops = {'horizontalalignment': 'left', 'verticalalignment': 'center', 'fontsize': fontsize}
+    textops.update(textops_update)
+    if ax is None: ax = plt.gca()
+    textops['transform'] = ax.transAxes
+    kwargs.update({'line_0':line_0,'line_1': line_1, 'line_2': line_2})
+    for i in range(len(kwargs)):
+        y = top_y - i*delta_y
+        t = kwargs.get('line_' + str(i))
+        if t is not None:
+            ax.text(left_x, y, t, **textops)
